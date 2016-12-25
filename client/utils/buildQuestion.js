8fc70794d54e21b2214b4c69966b26941b781768
Juan La Jara <wxomb@Juans-MacBook.local>
2016-12-25 11:49:42 -0500

    add icons to questions view

--- client/utils/buildQuestion.js
+++ client/utils/buildQuestion.js
@@ -6,7 +6,8 @@ module.exports = function ( question, agencies ) {
 	moment.tz.setDefault('America/Lima');
 	domChunk = h('div', { attributes: { 'data-id': question.id, 'class': 'question js-question', 'data-title': question.id } }
 	            , [ h('a', { attributes: { 'href': '/solicitudes/' + question.id + '/' + getSlug( question.title ) } }
-	                 , [ h('h4', [ question.title ] )
+	                 , [ h('span.pi-icon.pi-icon-' + question.status, [ ' ' ] )
+	                   , h('h4', [ question.title ] )
 	                   , h('span', [ agencies[ question.agencyId ] || question.agencyId ] )
 	                   , h('p', [ moment( question.createdAt ).fromNow() ] )
 	                   ]
--- methods/addViewPropertiesToQuestionModel.js
+++ methods/addViewPropertiesToQuestionModel.js
@@ -2,8 +2,10 @@ var getSlug = require( 'speakingurl' );
 var moment  = require( 'moment' );
 var tz      = require( 'moment-timezone' );
 
+var config  = require( '../config' );
+
 module.exports = function ( question ) {
 
-	question.slug          = getSlug( question.title );
-	question.formattedDate = moment( question.createdAt ).fromNow();
+	question.slug            = getSlug( question.title );
+	question.formattedDate   = moment( question.createdAt ).fromNow();
 };
