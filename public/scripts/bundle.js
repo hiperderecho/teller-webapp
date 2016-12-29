bdf25b8ba90c166eb9e89572179c7abe94c4bc33
Juan La Jara <wxomb@Juans-MacBook.local>
2016-12-29 16:14:03 -0500

    add minor styling changes

--- public/scripts/bundle.js
+++ public/scripts/bundle.js
@@ -822,9 +822,9 @@ module.exports = function ( question, agencies ) {
 	moment.tz.setDefault('America/Lima');
 	domChunk = h('div', { attributes: { 'data-id': question.id, 'class': 'question js-question', 'data-title': question.id } }
 	            , [ h('a', { attributes: { 'href': '/solicitudes/' + question.id + '/' + getSlug( question.title ) } }
-	                 , [ h('span.pi-icon.pi-icon-' + question.status, [ ' ' ] )
+	                 , [ h('span.pi-icon.pi-icon-lg.pi-icon-' + question.status, [ ' ' ] )
 	                   , h('h4', [ question.title ] )
-	                   , h('span', [ agencies[ question.agencyId ] || question.agencyId ] )
+	                   , h('span.for', [ agencies[ question.agencyId ] || question.agencyId ] )
 	                   , h('p', [ moment( question.createdAt ).fromNow() ] )
 	                   ]
 	                 )
--- routes/onBrowseQuestionsGetRequest.js
+++ routes/onBrowseQuestionsGetRequest.js
@@ -48,18 +48,4 @@ module.exports = function ( request, response ) {
 			response.status(500).render( '500', { error: error.stack } );
 		} );
 	} );
-
-	// methods.getBrowseQuestionsViewResources()
-	// .then( function ( result ) {
-	// 	var parsedAgencies;
-
-	// 	parsedAgencies = methods.parseAgenciesForInterpolationFromAgenciesModel( result.agencies );
-	// 	result.questions.forEach( methods.addViewPropertiesToQuestionModel );
-
-	// 	response.render( 'browseQuestions', { apiUrl: config.webapp.apiUrl, questions: result.questions, parsedAgencies: parsedAgencies, agencies: result.agencies } );
-	// } )
-	// .catch( function ( error ) {
-
-	// 	response.status(500).render( '500', { error: error.stack } );
-	// } );
 };
